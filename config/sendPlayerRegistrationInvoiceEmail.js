bb30fc986974d15135bc0386a111f95d54f2023c
rakib-netmow <rakib.netmow@gmail.com>
2023-12-13 14:12:23 +0600

    add internal css

--- config/sendPlayerRegistrationInvoiceEmail.js
+++ config/sendPlayerRegistrationInvoiceEmail.js
@@ -30,6 +30,134 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
               overflow-x: auto;
             }
           }
+    
+          body {
+            font-family: sans-serif;
+            background-color: #ffff;
+          }
+    
+          .container {
+            max-width: 60%;
+            margin: auto;
+            padding: 1rem;
+          }
+    
+          /* Breadcrumb */
+          nav {
+            color: #718096;
+            font-size: 0.875rem;
+            margin-bottom: 1rem;
+          }
+    
+          nav a {
+            color: #3490dc;
+            text-decoration: none;
+          }
+    
+          nav a:hover {
+            text-decoration: underline;
+          }
+    
+          /* Invoice Card */
+          .invoice-card {
+            background-color: #fff;
+            box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+            border-radius: 6px;
+            padding: 1rem;
+            margin-bottom: 1rem;
+          }
+    
+          /* Header */
+          .header {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
+            margin-bottom: 0.5rem;
+          }
+    
+          .logo {
+            margin-bottom: 0.5rem;
+          }
+    
+          .billing-address {
+            display: flex;
+            flex-direction: column;
+            width: 40%;
+            justify-content: end;
+            text-align: start;
+            margin-bottom: 0.5rem;
+          }
+    
+          /* Invoice Details */
+          .invoice-details {
+            margin-bottom: 0.5rem;
+          }
+    
+          .invoice-details p {
+            font-size: 0.875rem;
+            margin-bottom: 0;
+          }
+    
+          .invoice-details .font-semibold {
+            font-weight: 600;
+          }
+    
+          /* Table */
+          .table-container {
+            margin-bottom: 1rem;
+          }
+    
+          .scrollable-table {
+            overflow-x: auto;
+          }
+    
+          table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid #cbd5e0;
+          }
+    
+          th,
+          td {
+            border: 1px solid #cbd5e0;
+            padding: 0.5rem;
+            text-align: center;
+          }
+    
+          th {
+            background-color: #edf2f7;
+            font-size: 0.75rem;
+          }
+    
+          td {
+            font-size: 0.875rem;
+          }
+    
+          /* Totals */
+          .totals {
+            border-top: 1px solid #cbd5e0;
+            border-bottom: 1px solid #cbd5e0;
+            padding: 1rem 0;
+          }
+    
+          .total-particle {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: end;
+            font-size: 0.875rem;
+          }
+    
+          .totals .text-end {
+            text-align: start;
+          }
+          .child-total {
+            display: flex;
+            width: 100%;
+            justify-content: end;
+          }
         </style>
         <title>Your Invoice</title>
       </head>
@@ -60,21 +188,21 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                   </div>
     
                   <!-- Billing Address -->
-                  <div class="text-center md:text-right">
+                  <div class="text-center md:text-right billing-address">
                     <h6
                       class="text-muted text-uppercase text-base md:text-xl font-semibold mb-2 md:mb-0"
                     >
                       Billing Address
                     </h6>
-                    <p class="text-muted text-xs md:text-sm mb-1">${
-                      data?.name ? data?.name : "--"
-                    }</p>
-                    <p class="text-muted text-xs md:text-sm mb-1">${
-                      data?.email ? data?.email : "--"
-                    }</p>
-                    <p class="text-muted text-xs md:text-sm mb-1">${
-                      data?.city ? data?.city : "--"
-                    }</p>
+                    <p class="text-muted text-xs md:text-sm mb-1">
+                      ${data?.name ? data?.name : "--"}
+                    </p>
+                    <p class="text-muted text-xs md:text-sm mb-1">
+                      ${data?.email ? data?.email : "--"}
+                    </p>
+                    <p class="text-muted text-xs md:text-sm mb-1">
+                      ${data?.city ? data?.city : "--"}
+                    </p>
                     <p class="text-muted text-xs md:text-sm mb-1">
                       ${data?.address ? data?.address : "--"}, ${
       data?.zip ? " " + data?.zip : "--"
@@ -87,7 +215,7 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                 </div>
     
                 <!-- Invoice Details -->
-                <div class="mb-5">
+                <div class="mb-5 invoice-details">
                   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div>
                       <p
@@ -95,9 +223,7 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                       >
                         Invoice No
                       </p>
-                      <p class="text-xs md:text-sm mb-0">
-                        ${data?.invoiceId}
-                      </p>
+                      <p class="text-xs md:text-sm mb-0">${data?.invoiceId}</p>
                     </div>
                     <div>
                       <p
@@ -105,9 +231,7 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                       >
                         Date
                       </p>
-                      <p class="text-xs md:text-sm mb-0">
-                        ${data?.date}
-                      </p>
+                      <p class="text-xs md:text-sm mb-0">${data?.date}</p>
                     </div>
                     <div>
                       <p
@@ -116,9 +240,9 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                         Payment Status
                       </p>
                       <p class="text-xs md:text-sm mb-0 text-success font-semibold">
-                        <span class="text-center">${
-                          data?.status ? data?.status : "--"
-                        }</span>
+                        <span class="text-center"
+                          >${data?.status ? data?.status : "--"}</span
+                        >
                       </p>
                     </div>
                     <div>
@@ -127,16 +251,19 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                       >
                         Total Amount
                       </p>
-                      <p class="text-xs md:text-sm mb-0">$<span>$${
-                        data?.totalFees ? data?.totalFees : "00"
-                      } ${data?.currency ? " " + data?.currency : ""}</span></p>
+                      <p class="text-xs md:text-sm mb-0">
+                        $<span
+                          >$${data?.totalFees ? data?.totalFees : "00"}
+                          ${data?.currency ? " " + data?.currency : ""}</span
+                        >
+                      </p>
                     </div>
                   </div>
                 </div>
     
                 <!-- Table -->
-                <div class="mb-8 table-responsive">
-                  <div class="scrollable-table">
+                <div class="mb-8 table-responsive table-container">
+                  <div class="scrollable-table scrollable-table">
                     <table
                       class="table w-full border-collapse border border-gray-300"
                     >
@@ -154,9 +281,8 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                         </tr>
                       </thead>
                       <tbody>
-                      ${data?.data?.map(
-                        (d, i) =>
-                          `
+                        ${data?.data?.map(
+                          (d, i) => `
                         <tr class="text-xs md:text-sm text-center">
                           <td class="py-2">${i + 1}</td>
                           <td class="py-2">${
@@ -173,36 +299,42 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
                             d?.playerId ? d?.playerId : ""
                           }</td>
                           <td class="py-2">${d?.fees ? d?.fees : "--"}</td>
-                          <td class="py-2 bg-warning-subtle text-warning font-semibold">
+                          <td
+                            class="py-2 bg-warning-subtle text-warning font-semibold"
+                          >
                             ${d?.status ? d?.status : "--"}
                           </td>
                           <td class="py-2">${d?.fees ? d?.fees : "--"}</td>
                         </tr>
                         `
-                      )}
+                        )}
                       </tbody>
                     </table>
                   </div>
                 </div>
     
                 <!-- Totals -->
-                <div class="border-t border-b border-gray-200 py-4">
-                  <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-                    <div class="text-sm md:text-base">Sub Total :</div>
-                    <div class="text-sm md:text-base text-end">${
-                      data?.SubTotal ? data?.SubTotal : "--"
-                    }</div>
-                    <div class="text-sm md:text-base">GST (1 %) :</div>
-                    <div class="text-sm md:text-base text-end">${
-                      data.gst ? data?.gst : "00"
-                    }</div>
+                <div class="border-t border-b border-gray-200 py-4 totals">
+                  <div class="grid grid-cols-2 md:grid-cols-4 gap-4 total-particle">
+                    <div class="child-total">
+                      <div class="text-sm md:text-base">Sub Total :</div>
+                      <div class="text-sm md:text-base text-end">
+                        ${data?.SubTotal ? data?.SubTotal : "--"}
+                      </div>
+                    </div>
+                    <div class="child-total">
+                      <div class="text-sm md:text-base">GST (1 %) :</div>
+                      <div class="text-sm md:text-base text-end">
+                        ${data.gst ? data?.gst : "00"}
+                      </div>
+                    </div>
                     <div
-                      class="border-top border-top-dashed text-base md:text-xl col-span-2 md:col-span-3"
+                      class="child-total border-top border-top-dashed text-base md:text-xl col-span-2 md:col-span-3"
                     >
                       <div>Total Amount :</div>
-                      <div class="text-end">${
-                        data?.totalFees ? data?.totalFees : "00"
-                      }</div>
+                      <div class="text-end">
+                        ${data?.totalFees ? data?.totalFees : "00"}
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -216,7 +348,8 @@ const sendPlayerRegistrationInvoiceEmail = (email, data) => {
           defer
         ></script>
       </body>
-    </html>`,
+    </html>
+    `,
   };
 
   transpoter.sendMail(mailOption, async (error, info) => {
